757335f961882f95a3674797dcda062cfb554a3c
MoizSheraz <100522059+moizsheraz@users.noreply.github.com>
2023-05-07 22:52:00 +0500

    StartingArrays..

---
+++ All About Arrays and Algo/Main.java
@@ -0,0 +1,20 @@
+public class Main{
+
+public static void main(String[] args) {
+    // General Syntax
+    // data_type[] variable_name=new data_type[size];
+    //  we Also write 
+    // data_type variable_name={elments...}
+    //  internal working of array
+    // when i declared 
+    // int name[] reference variable create in Stack memory now when name=new name[]
+    int[] name;
+    name=new int[5];
+    // Defination of Array:Continous form of data but Actually in java data store in heap memory is actually not not continous bcz objects store in heap memory is not continous
+    // In Java, objects stored in heap memory are managed by a memory allocation system that dynamically allocates and deallocates memory as needed, which may result in non-contiguous memory allocation.
+    // accessing elements is same as in other programming language 
+    // System.out.println(name[3]);  // Output 0
+    
+}
+
+}
