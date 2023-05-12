7cd03cd5e1328eec7c089209b1971fb789bf7104
MoizSheraz <100522059+moizsheraz@users.noreply.github.com>
2023-05-12 18:12:21 +0500

    Completed:ArrayandArrayList

--- Arrays&ArrayList/PracticePrograms.java
+++ Arrays&ArrayList/PracticePrograms.java
@@ -1,18 +1,29 @@
+import java.util.Arrays;
+
 public class PracticePrograms {
     public static void main(String[] args) {
         // Swaping two numbers
-        int a=87;
-        int b=67;
-       swap( a,b); 
+        // int a=87;
+        // int b=67;
+    //    swap( a,b); 
        int [] arr={1,2,3,4,5,6,7,8,9,10};
-       
-       for()
+       int a=0;
+       int b=arr.length-1;
+       Reverse(arr, a, b);
+       System.out.println(Arrays.toString(arr));
     }
-    static void swap(int a,int b){
-int temp=b;
-b=a;
-a=temp;
-System.out.println("The value of a is " + a + " and the value of b is " + b);
+    static void swap( int []arr,int start,int end){
+int temp = arr[start];
+arr[start] = arr[end];
+arr[end] = temp;
+    }
+    static void Reverse( int[] arr,int start,int end){
+    while(start<end){
+        swap(arr,start,end);
+        start++;
+        end--;
+    }
+
     }
 
 }
